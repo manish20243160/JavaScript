1b3f77327922d3f3d95f401010da9e9b351a1353
manish20243160 <chaudharymanish70973@gmail.com>
2026-05-29 13:32:19 +0000

    about variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=14456
+let accountEmail="manish@gmail.com"
+var accountPassword="12334"
+accountCity="Jaipur"
+let accountState
+
+accountEmail="efjhebfh@gmail.com"
+accountPassword="89685069"
+accountCity="Banglore"
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
