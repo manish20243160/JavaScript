07626b8736d66c3f232539edc2ca881184e2a97c
manish20243160 <chaudharymanish70973@gmail.com>
2026-06-05 12:19:45 +0000

    Add conversion operations example

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,21 @@ let someNumber=45
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+//*******************Operations*********************
+
+let value =3
+let negValue = -value
+console.log(negValue)
+
+let str1="manish"
+let str2="chaudhary"
+
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+ 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
