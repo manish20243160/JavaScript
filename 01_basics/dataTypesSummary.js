fb9d60b0e84f95c2d03c070012e968e6de8be056
manish20243160 <chaudharymanish70973@gmail.com>
2026-06-08 11:38:43 +0000

    Stack and Heap learing

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -23,3 +23,29 @@ let myObj={
 const myFunction=function(){
     console.log("Hello world");
 }
+
+
+
+//+++++++++++++++++++++++++++++++
+
+//Stack(Primitive) , Heap(Non Primitive)
+
+let myYoutubeName="XYZdotcom"
+
+let anothername=myYoutubeName
+anothername="chaiandcode"
+
+console.log(myYoutubeName)
+console.log(anothername)
+
+let userOne={
+    email: "user@google.com",
+    upiid: "user@bl"
+}
+
+let userTwo=userOne
+
+userTwo.email= "manish@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
