3369a9bc9394c7f48db8801e9c8f5e4ced14bf32
manish20243160 <chaudharymanish70973@gmail.com>
2026-06-10 07:33:19 +0000

    Dates in JavaScript

---
+++ 01_basics/07Dates.js
@@ -0,0 +1,11 @@
+let myDate=new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
+let myCreateDate=new Date(2023, 0, 23)
+console.log( myCreateDate.toDateString())
+
+let newDate=new Date()
+console.log(newDate)
+console.log(newDate.getMonth()+1)
