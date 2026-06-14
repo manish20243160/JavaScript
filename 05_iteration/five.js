af1fb9c847b6aa43674c6bdebb3e44ef61e84827
manish20243160 <chaudharymanish70973@gmail.com>
2026-06-14 11:24:07 +0000

    for each loop

---
+++ 05_iteration/five.js
@@ -0,0 +1,11 @@
+//for each loop
+
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach( function (val){
+      console.log(val)
+} )
+
+coding.forEach( (item)=> {
+      console.log(item)
+} )
